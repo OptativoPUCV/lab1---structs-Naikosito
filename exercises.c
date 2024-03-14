9b21482a9209e799bee628d44bea89d08aaef80e
Your Name <you@example.com>
2024-03-14 20:48:39 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -61,17 +61,15 @@ arreglos en un tercer arreglo también ordenado.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[]) 
 {
-  result = (int *)malloc((size1 + size2) * sizeof(int));
-
-  int i = 0, j = 0, k = 0;
+  int *result = (int *)malloc((size1 + size2) * sizeof(int));
   
-  while (i < size1 && j < size2) {
-      if (arr1[i] < arr2[j]) {  
-            result[k++] = arr1[i++];
-      } else {
-            result[k++] = arr2[j++];
-      }
-  }
+  int i = 0, j = 0, k = 0;
+
+  while (i < size1 && j < size2)
+    if (arr1[i] < arr2[j])
+      result[k++] = arr1[i++];
+    else
+      result[k++] = arr2[j++];
 
 
 }
